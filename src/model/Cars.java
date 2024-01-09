a4c9a6c820354800da036a6921a71843eb9d8474
baoht42 <snorlax4293@gmail.com>
2024-01-09 15:23:40 +0700

    tao class abstract Car va sub-class Toyota

---
+++ src/model/Cars.java
@@ -0,0 +1,61 @@
+package model;
+
+public abstract class Cars {
+    private String idCar;
+    private String nameCar;
+    private double cost;
+    private int seatOfCar;
+
+    public Cars(String idCar, String nameCar, double cost, int seatOfCar) {
+        this.idCar = idCar;
+        this.nameCar = nameCar;
+        this.cost = cost;
+        this.seatOfCar = seatOfCar;
+    }
+
+    public Cars() {
+    }
+
+    public String getIdCar() {
+        return idCar;
+    }
+
+    public void setIdCar(String idCar) {
+        this.idCar = idCar;
+    }
+
+    public String getNameCar() {
+        return nameCar;
+    }
+
+    public void setNameCar(String nameCar) {
+        this.nameCar = nameCar;
+    }
+
+    public double getCost() {
+        return cost;
+    }
+
+    public void setCost(double cost) {
+        this.cost = cost;
+    }
+
+    public int getSeatOfCar() {
+        return seatOfCar;
+    }
+
+    public void setSeatOfCar(int seatOfCar) {
+        this.seatOfCar = seatOfCar;
+    }
+    public abstract double getPrice();
+
+    @Override
+    public String toString() {
+        return "Cars{" +
+                "idCar='" + idCar + '\'' +
+                ", nameCar='" + nameCar + '\'' +
+                ", cost=" + cost +
+                ", seatOfCar=" + seatOfCar +
+                '}';
+    }
+}
---
+++ src/model/Toyota.java
@@ -0,0 +1,29 @@
+package model;
+
+public class Toyota extends Cars{
+    private int dayRent = 0;
+
+    public Toyota(String idCar, String nameCar, double cost, int seatOfCar, int dayRent) {
+        super(idCar, nameCar, cost, seatOfCar);
+        this.dayRent = dayRent;
+    }
+
+    public Toyota(int dayRent) {
+        this.dayRent = dayRent;
+    }
+    public Toyota() {
+
+    }
+
+    @Override
+    public double getPrice() {
+        if (getSeatOfCar()==4){
+            return getCost()*dayRent;
+        }
+        if (getSeatOfCar()>4 && getSeatOfCar()<7){
+            return getCost()*dayRent + 200;
+        }
+        else
+            return 0;
+    }
+}
